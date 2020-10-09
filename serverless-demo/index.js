58b5b525d848615d2c3ecbab23a0cf428e6ee69a
shivasaitechatcore <shivasai@techatcore.com>
2020-10-09 17:00:02 +0530

    Add a change in index.js file

--- serverless-demo/index.js
+++ serverless-demo/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.http = (request, response) => {
-  response.status(200).send('Hello this is vivek commit ..... !');
+  response.status(200).send('Hello this is shivasai... !');
 };
 
 exports.event = (event, callback) => {
